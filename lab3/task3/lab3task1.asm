088cc03a5667e9e08113d0b4d658ba3d78652464
Bruthzilla <ruth.dirnfeld@gmail.com>
2017-09-26 16:44:50 +0200

    Update lab3task1.asm

--- lab3/task3/lab3task1.asm
+++ lab3/task3/lab3task1.asm
@@ -3,7 +3,7 @@
 ; Date: 2017-09-25
 ; Author:
 ; Student name 1 Ruth Dirnfeld
-; Student name 2 Alexandra Bj‰remo
+; Student name 2 Alexandra Bj√§remo
 ;
 ; Lab number: 3
 ; Title: Interrupts.
@@ -25,16 +25,15 @@
 ;
 ;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-
-.include"m2560def.inc"
+.include "m2560def.inc"
 
 .org 0x00
 rjmp start
 
 .org INT0addr
-rjmp interrupt_0
+jmp interrupt_0
 .org INT1addr
-rjmp interrupt_1
+jmp interrupt_1
 
 .org 0x72
 
@@ -47,8 +46,10 @@ out SPL, r16 ; store in SPL
 ldi r20, 0x00
 out DDRD, r20			; all zero's to DDRD, input
 
-ldi r20, 0xFF
-out DDRB, r20			; all one's to DDRB, outputs
+ldi r22, 0xFF
+out DDRB, r22			; all one's to DDRB, outputs
+
+ldi r23, 0xD0
 
 ldi r20, 0x03			; INT0 and INT1 enabled
 out EIMSK, r20
@@ -58,17 +59,62 @@ sts EICRA, r20
 
 sei						; global interrupt enable
 
-ldi r20, 0xF7
-out PORTB, r20
 
 main:
-nop
+
+ldi r22, 0x3C
+out PORTB, r22
+cpi r17, 0x00
+breq left
+rjmp right
+
 rjmp main
 
-interrupt_0:			
+left:
+ldi r22, 0xEC
+out PORTB, r22
+rol r22
+add r22, r23
+out PORTB, r22
+rcall delay
+rjmp left
+
+right:
+ldi r22, 0x37
+out PORTB, r22
+com r22
+lsr r22
+com r22
+add r22, r23
+out PORTB, r22
+rcall delay
+rjmp right
+
+
+interrupt_0:
+ldi r17, 0xFF
+ldi r22, 0x37
+out PORTB, r22
 interrupt_1:
-andi r20, 0x08			; isolating the bit corresponding to LED3
-com r20					; inverting the bit
-out PORTB, r20			; outing to LED3
-reti					; return from interrupt
+ldi r17, 0x00
+ldi r22, 0xCE
+out PORTB, r22			
+
+reti			
+
+delay:
+	ldi  r18, 3
+    ldi  r19, 138
+    ldi  r21, 86	
+L1:	dec  r21
+    brne L1
+	dec  r19
+    brne L1
+    dec  r18
+    brne L1
+    rjmp PC+1
+
+ret
+
+
 
