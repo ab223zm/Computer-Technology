c20906a074a5ba59353209690112ff69be9e2730
Bruthzilla <ruth.dirnfeld@gmail.com>
2017-09-26 08:37:43 +0200

    Update lab3task3.asm

--- temp/lab3mon/lab3task3.asm
+++ temp/lab3mon/lab3task3.asm
@@ -3,7 +3,7 @@
 ; Date: 2017-09-25
 ; Author:
 ; Student name 1 Ruth Dirnfeld
-; Student name 2 Alexandra Bj‰remo
+; Student name 2 Alexandra Bj√§remo
 ;
 ; Lab number: 3
 ; Title: Interrupts.
@@ -31,20 +31,21 @@
 rjmp start
 
 .org INT0addr
-rjmp interrupt_0
-/*.org INT1addr
-rjmp interrupt_1
-*/
+jmp interrupt_0
+
+.org INT1addr
+jmp interrupt_1
+
 .org 0x72
 
 start:
-ldi r16, HIGH(RAMEND) ; MSB part av address to RAMEND
-out SPH, r16 ; store in SPH
-ldi r16, LOW(RAMEND) ; LSB part av address to RAMEND
-out SPL, r16 ; store in SPL
+ldi r16, HIGH(RAMEND) 
+out SPH, r16 
+ldi r16, LOW(RAMEND) 
+out SPL, r16 
 
-ldi r22, 0x00
-out DDRD, r22			; all zero's to DDRD, input
+ldi r20, 0x00
+out DDRD, r20			; all zero's to DDRD, input
 
 ldi r20, 0xFF
 out DDRB, r20			; all one's to DDRB, outputs
@@ -52,46 +53,82 @@ out DDRB, r20			; all one's to DDRB, outputs
 ldi r20, 0x03			; INT0 and INT1 enabled
 out EIMSK, r20
 
-ldi r20, 0x08			; INT1 falling edge, INT0 rising edge
-sts EICRA, r20
+ldi r20, 0x15			; INT1 falling edge, INT0 rising edge
+sts EICRA, r20	
 
-sei						; global interrupt enable
+sei				; global interrupt enable
+
+ldi r16, 0x00		
+ldi r18, 0x00		
+ldi r21, 0x00		
+ldi r22, 0x00		
 
 main:
+	cpi r16, 0x00
+	breq right		; if right-button is pressed
+	cpi r22, 0x00
+	breq left		; if left-button is pressed
 
-ldi r20, 0x3C
-out PORTB, r20
+	ldi r20, 0b11000011     ;normal state of left and right side
+	out DDRB, r20
+	rjmp main
 
-ldi r17, 0x04
+left:
+	ldi r20, 0x10           ;loading with 0001_0000
+	loop1:
+		cpi r20, 0x00
+		breq left
 
-rjmp main
+		mov r17, r16
+		ldi r18, 0x03   ;loading with 0000_0011
+		add r17, r18 
 
-interrupt_0:
-	cpi r20, 0x3F				; checking if all LEDs are off
-	breq equal	
-	add r20, r17
-	ror r17			
-	out PORTB, r20				; write in PORTB, turning on LEDs
-	;rcall delay
-reti
+		out DDRB, r17
+		;rcall delay1
 
-	equal: 
-	ldi r20, 0x37			
+		lsl r20
+		rjmp loop1
 
-rjmp main
+right:
+	ldi r20, 0x08            ;loading with 0000_1000
+	loop2:
+		cpi r20, 0x00
+		breq right
 
-delay:
+		mov r17, r20
+		ldi r18, 0xC0    ; loading with 1100_0000
+		add r17, r18 
+
+		out DDRB, r17
+		;rcall delay1
+
+		lsr r20
+		rjmp loop2
+
+delay1:
 	
-    ldi  r18, 3
-    ldi  r19, 138
-    ldi  r21, 86
-L1: dec  r21
-    brne L1
-    dec  r19
-    brne L1
-    dec  r18
-    brne L1
-    rjmp PC+1
-ret
+	ldi r16, 2		; innner count
+	ldi r17, 2		; middle count
+	ldi r18, 2		; outer count
+
+	delay2:
+
+		dec r18
+		brne delay2
+
+		dec r17
+		brne delay2
+
+		dec r20
+		brne delay2
+		nop
+	ret
+
+interrupt_0:			
+	com r16			; inverting
+	reti
+interrupt_1:			
+	com r22
+	reti
 
 
